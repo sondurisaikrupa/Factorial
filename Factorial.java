cf9154e72fd26c18c3e62551541b418214cc3717
sondurisaikrupa <saikrupa303@gmail.com>
2016-08-09 15:27:53 +0530

    First Commit

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int fact=1;
+		int number=5;
+		for(i=1;i<number;i++){
+			fact= fact*i;
+		}
+		System.out.println("Factorial of a number:"+number+"is:"+fact);
+	}
+	
+	
+	
+}
